09a735f0f152a182e5370caa7ea490a06e8f75b7
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-03-09 12:07:04 -0800

    save() changes on put rating and status

--- routes/showRouter.js
+++ routes/showRouter.js
@@ -1,32 +1,35 @@
 const express = require("express")
 const router = express.Router()
-const { check, validationResult } = require("express-validator");
+const { check, validationResult } = require("express-validator")
 const { Show } = require("../models/Show")
-const { sequelize } = require("../config/db");
+const { sequelize } = require("../config/db")
 
 router.get("/", async (req, res) => {
-    res.send(await Show.findAll());
+    res.send(await Show.findAll())
 })
 
 router.get("/:id", async (req, res) => {
-    const show = await Show.findByPk(req.params.id);
-    res.send(show);
+    const show = await Show.findByPk(req.params.id)
+    res.send(show)
 })
 
+// Does not work
 router.get("/:genre", async (req, res) => {
     const shows = await Show.findAll({
         where: {
             genre: req.params.genre
         }
-    });
-    res.send(shows);
+    })
+    console.log(shows)
+    res.send(shows)
 })
 
 router.put("/rating/:id", async (req, res) => {
     try {
-        const show = await Show.findByPk(req.params.id);
-        show.rating = req.body.rating;
-        res.send(await Show.findAll());
+        const show = await Show.findByPk(req.params.id)
+        show.rating = req.body.rating
+        show.save()
+        res.send(await Show.findAll())
     } catch {
         res.sendStatus(404)
     }
@@ -34,18 +37,19 @@ router.put("/rating/:id", async (req, res) => {
 
 router.put("/status/:id", async (req, res) => {
     try {
-        const show = await Show.findByPk(req.params.id);
-        show.status = req.body.status;
-        res.send(await Show.findAll());
+        const show = await Show.findByPk(req.params.id)
+        show.status = req.body.status
+        show.save()
+        res.send(await Show.findAll())
     } catch {
         res.sendStatus(404)
     }
 })
 
 router.delete("/:id", async (req, res) => {
-    const show = await Show.findByPk(req.params.id);
-    show.destroy();
-    res.send(await Show.findAll());
+    const show = await Show.findByPk(req.params.id)
+    show.destroy()
+    res.send(await Show.findAll())
 })
 
 module.exports = router
