ec064853c123a2aa44fb9a09a60ccb2df9a55cf6
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-03-09 12:18:23 -0800

    added validation to show/status

--- routes/showRouter.js
+++ routes/showRouter.js
@@ -16,7 +16,6 @@ router.get("/:id", async (req, res) => {
         res.send(show)
 })
 
-// Does not work
 router.get("/genre/:genre", async (req, res) => {
     const shows = await Show.findAll({
         where: {
@@ -37,16 +36,24 @@ router.put("/rating/:id", async (req, res) => {
     }
 })
 
-router.put("/status/:id", async (req, res) => {
-    try {
-        const show = await Show.findByPk(req.params.id)
-        show.status = req.body.status
-        show.save()
-        res.send(await Show.findAll())
-    } catch {
-        res.sendStatus(404)
-    }
-})
+router.put("/status/:id",
+    [check("status").not().isEmpty().trim()],
+    [check("status").isLength({ min: 5, max: 25 })],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            res.status(400).json({ error: errors.array() })
+            return
+        }
+        try {
+            const show = await Show.findByPk(req.params.id)
+            show.status = req.body.status
+            show.save()
+            res.send(await Show.findAll())
+        } catch {
+            res.sendStatus(404)
+        }
+    })
 
 router.delete("/:id", async (req, res) => {
     const show = await Show.findByPk(req.params.id)
